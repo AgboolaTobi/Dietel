4abd441d19272e3f891ea46ebf5e4e78ebe973a4
Agboola Tobi Samuel <tobi4tee@gmail.com>
2023-09-12 17:22:24 +0100

    Commtted again !

--- src/classExercises/ClassExercise.java
+++ src/classExercises/ClassExercise.java
@@ -1,2 +1,10 @@
-package classExercises;public class ClassExercise {
+package classExercises;
+
+public class ClassExercise {
+    public static void main(String[] args) {
+        int numberOne = 15;
+        int numberTwo = 10;
+        int sum = numberOne + numberTwo;
+        System.out.println(sum);
+    }
 }
