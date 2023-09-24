fd14f37ae6cb2ab2231c559f8f63decfa7fa7c49
Agboola Tobi Samuel <tobi4tee@gmail.com>
2023-09-24 21:16:04 +0100

    Made this commit on saturday after I completed the python assignment in java

--- src/myPythonExercisesInJava/HalfDiamond.java
+++ src/myPythonExercisesInJava/HalfDiamond.java
@@ -4,13 +4,13 @@ public class HalfDiamond {
     public static void main(String[] args) {
         int i ;
         int j ;
-        for (i = 0; i <= 5; i++) {
-            for (j = 0; j <= i ; j++) {
+        for (i = 1; i <= 5; i++) {
+            for (j = 1; j <= i ; j++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
-        for (i = 0; i <= 4; i++) {
+        for (i = 1; i <= 4; i++) {
             for (j = 4; j >= i ; j--) {
                 System.out.print("* ");
             }
